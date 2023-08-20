ec257cf9e0487fdfe18b29f27f7364607dbfd79f
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-20 22:17:39 +0700

    feat: Hero

--- app/component/Main.js
+++ app/component/Main.js
@@ -1,8 +1,10 @@
+import Hero from "./Hero";
+
 export default function Main() {
     return (
         <main className="sm:container mx-auto border">
-            <div className="pt-32 pb-16">
-                <h1 className="font-bold text-4xl">Hello world!</h1>
+            <div className="md:pt-40 pt-28 md:pb-20 pb-14">
+                <Hero />
             </div>
         </main>
     );
